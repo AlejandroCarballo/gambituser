c520167b654cfac37155a36a000ddb18b1de9114
AlejandroCarballo <o.alejandro.carballo@gmail.com>
2023-07-09 20:02:30 -0300

    ultimo commit

--- bd/signup.go
+++ bd/signup.go
@@ -17,7 +17,7 @@ func SignUp(sig models.SignUp) error {
 
 	defer Db.Close()
 
-	sentencia := "INSERT INTO  users (User_Email, User_UUID, User_DateAdd) VALUES ('" + sig.UserEmail + "','" + sig.UserUUID + "','" + tools.FechaMySql() + "')"
+	sentencia := "INSERT INTO  users (User_Email, User_UUID, User_DateAdd) VALUES (" + sig.UserEmail + "','" + sig.UserUUID + "','" + tools.FechaMySql() + "')"
 	fmt.Println(sentencia)
 
 	_, err = Db.Exec(sentencia)
